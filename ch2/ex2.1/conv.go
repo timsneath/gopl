55c4079424cecd909f1c17225814b067f15d2875
Tim Sneath <tim@sneath.org>
2023-02-21 15:58:42 -0800

    Update conversion

--- ch2/ex2.1/conv.go
+++ ch2/ex2.1/conv.go
@@ -1,5 +1,11 @@
 package tempconv
 
+const (
+	AbsoluteZeroC  Celsius = -273.15
+	FreezingWaterC Celsius = 0
+	BoilingWaterC  Celsius = 100
+)
+
 func CtoF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
 func CtoK(c Celsius) Kelvin     { return Kelvin(c - 273.15) }
 func FtoC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
--- ch2/ex2.2/ctof.go
+++ ch2/ex2.2/ctof.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 
-	tempconv "sneath.org/gopl/ch2/ex2.1"
+	temp "sneath.org/gopl/ch2/ex2.1"
 )
 
 func main() {
-	fmt.Println(tempconv.Celsius(100))
+	fmt.Println(temp.Celsius(100))
+	fmt.Printf("%v\n", temp.Fahrenheit(32))
+	fmt.Println(temp.Fahrenheit(temp.CtoF(temp.BoilingWaterC)))
 }
