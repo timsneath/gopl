0adaf751376bb371dfd07895d22f00a945508907
Tim Sneath <tim@sneath.org>
2023-02-22 17:54:22 -0800

    Playing w/ color

--- ch3/ex3.6/main.go
+++ ch3/ex3.6/main.go
@@ -6,6 +6,8 @@ import (
 	"image/png"
 	"math/cmplx"
 	"os"
+
+	"github.com/muesli/gamut"
 )
 
 func main() {
@@ -14,30 +16,35 @@ func main() {
 		width, height          = 1024, 1024
 	)
 
+	palette, _ := gamut.Generate(50, gamut.PastelGenerator{})
 	img := image.NewRGBA(image.Rect(0, 0, width, height))
 	for py := 0; py < height; py++ {
 		y := float64(py)/height*(ymax-ymin) + ymin
 		for px := 0; px < width; px++ {
 			x := float64(px)/width*(xmax-xmin) + xmin
 			z := complex(x, y)
-			img.Set(px, py, mandelbrot(z))
+
+			iterations := mandelbrot(z)
+			if iterations == -1 {
+				img.Set(px, py, color.Black)
+			} else {
+				img.Set(px, py, palette[iterations%len(palette)])
+			}
 		}
 	}
 	png.Encode(os.Stdout, img)
 }
 
-func mandelbrot(z complex128) color.Color {
+func mandelbrot(z complex128) int {
 	const iterations = 200
 
 	var v complex128
 	for n := uint8(0); n < iterations; n++ {
 		v = v*v + z
 		if cmplx.Abs(v) > 2 {
-			Cb := int(float64(n)/100*65536) / 256
-			Cr := int(float64(n)/100*65536) % 256
-
-			return color.YCbCr{Y: 127, Cb: uint8(Cb), Cr: uint8(Cr)}
+			return int(n)
 		}
+
 	}
-	return color.Black
+	return -1
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module sneath.org/gopl
 
 go 1.20
+
+require github.com/muesli/gamut v0.3.1
+
+require (
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/muesli/clusters v0.0.0-20200529215643-2700303c1762 // indirect
+	github.com/muesli/kmeans v0.3.1 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+)
